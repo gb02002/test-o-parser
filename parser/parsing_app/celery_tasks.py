40374ca4b403e241577e6607c70ec82c7b4581ea
Gleb B. <baburin899@gmail.com>
2024-05-25 23:28:31 +0200

    parser created

--- parser/parsing_app/celery_tasks.py
+++ parser/parsing_app/celery_tasks.py
@@ -1,16 +1,13 @@
-import os
-import smtplib
-from email.message import EmailMessage
-from datetime import datetime
-from celery import Celery, shared_task
-from parsing_app.models import Products
+from celery import Celery
+
+from parsing_app.core import main_logic
 
 from parser.settings import REDIS_HOST, REDIS_PORT
 
-celery = Celery('tasks', broker=f'redis://{REDIS_HOST}:{REDIS_PORT}')
+celery = Celery('celery_tasks', broker=f'redis://{REDIS_HOST}:{REDIS_PORT}')
 
 
 @celery.task
 def start_parsing(amount_of_products):
     """Main logic"""
-    pass
+    main_logic(amount_of_products)
